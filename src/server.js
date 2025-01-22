896d3aa55cc6b7b43368cb4a3b04fd92c7d23442
MrKalleP <kallle.v.pettersson@gmail.com>
2025-01-22 10:53:29 +0100

    fixed cors for conecting to the frontend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,31 @@
-const app = require("./app");
 const portNumber = 3000;
+
+const cors = require("cors")
+const express = require("express");
+const app = express()
+
+const projectsRoutes = require("./routes/projectsRoutes");
+const logsRoutes = require("./routes/logsRoutes");
+
+app.use(express.json());
+app.use(cors())
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
+
+// Routes 
+app.use("/projects", projectsRoutes);
+app.use("/logs", logsRoutes);
+
+// Default route / homepage
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+module.exports = app;
 
 app.listen(portNumber, () => {
     console.log(`Server is running on http://localhost:${portNumber}`);
