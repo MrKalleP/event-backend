3b571ddb908ebd363427bcb76c0933b6c01ca00d
MrKalleP <kallle.v.pettersson@gmail.com>
2025-01-30 13:14:00 +0100

    fixed the db back

--- src/routes/logsRoutes.js
+++ src/routes/logsRoutes.js
@@ -8,8 +8,8 @@ router.get("/type/:type", getLogsByType);
 // http://localhost:3000/logs/type/error eller info osv jag får alla errors från alla project
 router.get("/", getAllLogs);
 // http://localhost:3000/logs to get all logs jag får alla loggar från alla project
-router.get("/project/:projectId/type/:type", getProjectLogsByType); // jag får alla typer från just det projectet jag valt
-
+router.get("/project/:projectId/type/:type", getProjectLogsByType);
+// jag får alla typer från just det projectet jag valt
 
 module.exports = router;
 
