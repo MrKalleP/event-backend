c470a6a4472ce653d9f2f7b9742a5ac5941fb9f7
MrKalleP <kallle.v.pettersson@gmail.com>
2025-01-30 11:03:10 +0100

    testing some best practice

--- src/routes/logsRoutes.js
+++ src/routes/logsRoutes.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const { getProjectLogsId, getLogsByType, getAllLogs, getProjectLogsByType } = require("../controllers/logsController");
-const { getLogsLastDays, getLogsByTypeLastDays, getLogsByMultipleTypes, getLogCountPerProject } = require("../controllers/utilsController");
 const router = express.Router();
 
 router.get("/project/:projectId", getProjectLogsId);
-// http://localhost:3000/logs/project/2 
+// http://localhost:3000/logs/project/2 jag får alla project för just det projectet
 router.get("/type/:type", getLogsByType);
-// http://localhost:3000/logs/type/error eller info osv
+// http://localhost:3000/logs/type/error eller info osv jag får alla errors från alla project
 router.get("/", getAllLogs);
-// http://localhost:3000/logs to get all logs
-router.get("/project/:projectId/type/:type", getProjectLogsByType);
-
-// Nya routes
-router.get("/last/:date", getLogsLastDays);
-router.get("/type/:type/last/:date", getLogsByTypeLastDays);
-router.get("/types", getLogsByMultipleTypes);
-router.get("/count", getLogCountPerProject);
+// http://localhost:3000/logs to get all logs jag får alla loggar från alla project
+router.get("/project/:projectId/type/:type", getProjectLogsByType); // jag får alla typer från just det projectet jag valt
 
 
 module.exports = router;
